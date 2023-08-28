6aff83737dec6f41eb034d56e542645d0c935052
Lars Windolf <lars.windolf@gmx.de>
2023-08-29 01:40:01 +0200

    Better debugging

--- assets/js/bg-remove.js
+++ assets/js/bg-remove.js
@@ -9,25 +9,29 @@ import removeBackground from './../../node_modules/@imgly/background-removal/dis
 
 function backgroundRemove(id) {
         console.log(id);
+        const photoStatus = document.getElementById('photoStatus');
         const e = document.getElementById(id);
         const image_src = e.src;
 
         console.log("backgroundRemove()");
         return removeBackground(image_src, {
                 progress: (key, current, total) => {
-                        console.log(`Downloading ${key}: ${current} of ${total}`);
+                        if(-1 !== key.indexOf("fetch"))
+                                photoStatus.innerText = `Downloading model (${key}: ${current*100/total}%)...`;
+                        else
+                                photoStatus.innerText = `Removing background (${key}: ${current} / ${total})...`;
                 },
                 publicPath: 'node_modules/@imgly/background-removal/dist/',
                 proxyToWorker: true,
                 debug: true,
                 model: 'small'
         }).then((blob) => {
+                photoStatus.innerText = `Photo is ready.`;
                 // The result is a blob encoded as PNG. It can be converted to an URL to be used as HTMLImage.src
-                const url = URL.createObjectURL(blob);
-                console.log(url);
-                e.src = url;
+                e.src = URL.createObjectURL(blob);
         }).catch(function(err) {
-                console.error('Could not load image', err);
+                photoStatus.innerText = `Could not process image: ${err}`;
+                console.error('Could not process image', err);
         });
 }
 
